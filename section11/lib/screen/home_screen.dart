87c6364ded53f6017742f48c1fa2981ed8bc612e
yeopseung <kwak991021@gmail.com>
2023-07-13 13:21:46 +0900

    section11 #1

---
+++ section11/lib/screen/home_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const TestWidget(label: 'test1'),
+            const TestWidget(label: 'test2'),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {});
+                },
+                child: const Text('빌드!')),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TestWidget extends StatelessWidget {
+  final String label;
+
+  const TestWidget({super.key, required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    print('$label 빌드 실행');
+    return Container(
+      child: Text(
+        label,
+      ),
+    );
+  }
+}
